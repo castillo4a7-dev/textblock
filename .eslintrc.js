54b486d22f521cb4cd3baa79b3fdf4fab751d603
Kristopher Cargile <kris@kriscargile.com>
2024-09-09 19:44:51 -0600

    fix eslint versions; housekeeping

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,45 @@
 module.exports = {
-  "extends": [
-    "eslint:recommended", // identifies common problems; see https://eslint.org/docs/rules/
-    "plugin:prettier/recommended" // harmonizes Prettier and ESLint recommended rules; see https://prettier.io/docs/en/eslint.html#use-both
-  ],
-  "plugins": [
-    "prettier" // ensures .prettierrc is honored
-  ],
-  "env": {
-    "browser": true // lets ESLint know `window` and `document` are defined
-  }
-}
+	env: {
+		browser: true
+	},
+	extends: ["eslint:recommended"],
+	ignorePatterns: [".eslintrc.js", ".old", "dist"],
+	parser: "@typescript-eslint/parser",
+	plugins: ["@typescript-eslint", "import"],
+	overrides: [
+		{
+			files: ["**/*.ts", "**/*.js"],
+			extends: [
+				"plugin:@typescript-eslint/recommended",
+				"plugin:@typescript-eslint/recommended-requiring-type-checking"
+			],
+			rules: {
+				"@typescript-eslint/no-non-null-assertion": "off",
+				"@typescript-eslint/no-explicit-any": "off",
+				"@typescript-eslint/no-redundant-type-constituents": "off",
+				"@typescript-eslint/no-unused-expressions": "off",
+				"@typescript-eslint/naming-convention": [
+					"error",
+					{
+						selector: "interface",
+						format: ["PascalCase"],
+						custom: {
+							regex: "^I[A-Z]",
+							match: false
+						}
+					}
+				]
+			}
+		}
+	],
+	parserOptions: {
+		project: ["./tsconfig.json"],
+		tsconfigRootDir: __dirname
+	},
+	settings: {
+		"import/resolver": {
+			typescript: {}
+		}
+	},
+	root: true
+};
